156e76dbf676ee137cb35758bd044c27284b5a17
Stephen <kosi.kk2000@gmail.com>
2021-05-05 02:11:18 +0100

    JAVASCRIPT first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//FIRST ASSIGNMENT
+let A = 70, B = 60, C = 50, D = 40, F = "carryover";
+
+let studentA = A;
+
+let studentB = B;
+
+let studentcarry = 32;
+
+if (studentcarry === A){
+    console.log("Distinction, Sharp A")
+} else if (studentcarry === B && C && D) {
+    console.log("Fair Result,Try harder next time")
+} else {
+    console.log("Nwanne come back next year")
+}
+
+
+//SECOND ASSIGNMEMT
+for(i=0;i<=100;i+=2){
+    console.log(i)
+}
+
+
+//THIRD ASSIGNMENT
+let premierLeague = [
+    'Arsenal','Aston villa','Brighton','Burnley','Chelsea', 'Crystal palace','Everton','Fulham','Leeds',
+    'Leicester city','Liverpool','Manchester City','Manchester United','Newcastle United','Sheffield United',
+    'Southampton','Tottenham','Westbrom','West ham','Wolverhampton Wanderers'
+    ]
+
+    for( i = 0; i < premierLeague.length; i++){
+        console.log(premierLeague[i]);
+    }
